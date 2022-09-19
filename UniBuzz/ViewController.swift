d7ae7757d6a12e14b109c884283b9259a8c4ea3b
farhanalmasyhur <farhan.almasyhur@gmail.com>
2022-09-19 16:37:42 +0700

    feat: first commit

---
+++ UniBuzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniBuzz
+//
+//  Created by Muhammad Farhan Almasyhur on 19/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
