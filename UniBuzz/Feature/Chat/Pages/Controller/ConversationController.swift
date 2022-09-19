833b9ac214d3631bf51cc41f29e0ac4a45b30e0e
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-09-19 22:00:30 +0700

    feat: add initial tabbar UI

---
+++ UniBuzz/Feature/Chat/Pages/Controller/ConversationController.swift
@@ -0,0 +1,17 @@
+//
+//  ConversationController.swift
+//  UniBuzz
+//
+//  Created by Kevin ahmad on 19/09/22.
+//
+
+import UIKit
+
+class ConversationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
--- UniBuzz/Feature/TabBarViewController.swift
+++ UniBuzz/Feature/TabBarViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tabBar.backgroundColor = .white
+        self.tabBar.tintColor = .blue
+        configureViewControllers()
+        
+    }
+    
+    func navigationController(image: UIImage?, title: String, rootViewController: UIViewController) ->
+        UINavigationController{
+            let nav = UINavigationController(rootViewController: rootViewController)
+            nav.tabBarItem.image = image
+            nav.tabBarItem.title = title
+            nav.navigationBar.barTintColor = .black
+            return nav
+        }
+    
+    func configureViewControllers() {
+        let feeds = ConversationController()
+        let nav1 = navigationController(image: UIImage(systemName: "house"),title: "Feeds", rootViewController: feeds)
+        let search = ConversationController()
+        let nav2 = navigationController(image: UIImage(systemName: "magnifyingglass"),title: "Search", rootViewController: search)
+        let mission = ConversationController()
+        let nav3 = navigationController(image: UIImage(systemName: "doc.plaintext"), title: "Mission", rootViewController: mission)
+        let conversation = ConversationController()
+        let nav4 = navigationController(image: UIImage(systemName: "envelope"), title: "Chat", rootViewController: conversation)
+        viewControllers = [nav1,nav2,nav3,nav4]
     }
 }
