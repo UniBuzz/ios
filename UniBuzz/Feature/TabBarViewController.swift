3f348bac5ed4c247f6a92ce520492a81205064f3
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-09-19 17:54:29 +0700

    feat: add RXSwift and TabBarViewController

---
+++ UniBuzz/Feature/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  UniBuzz
+//
+//  Created by Kevin ahmad on 19/09/22.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
