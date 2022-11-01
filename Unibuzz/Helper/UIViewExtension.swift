0ef49addc3857e618a6658a67238a5732603d1d8
Farhan Almasyhur <51041665+FarhanAlmasyhur@users.noreply.github.com>
2022-11-01 15:34:02 +0700

    feat: layout first honey page

--- Unibuzz/Helper/UIViewExtension.swift
+++ Unibuzz/Helper/UIViewExtension.swift
@@ -19,3 +19,16 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
+
+extension UIView {
+    func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            self.alpha = 1.0
+    }, completion: completion)  }
+
+    func fadeOut(_ duration: TimeInterval = 0.5, delay: TimeInterval = 1.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            self.alpha = 0.3
+    }, completion: completion)
+   }
+}
